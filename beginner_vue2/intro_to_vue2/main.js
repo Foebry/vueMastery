e8e5998e26d692a75d9d44f89bc3f0448d6cd892
Sander Fabry <sander.fabry@yucopia.com>
2022-11-06 15:01:19 +0100

    5. event handling

--- beginner_vue2/intro_to_vue2/main.js
+++ beginner_vue2/intro_to_vue2/main.js
@@ -2,6 +2,7 @@ let app = new Vue({
   el: "#app",
   data: {
     product: "Boots",
+    variant: "",
     inStock: false,
     inventory: 8,
     onSale: true,
@@ -17,5 +18,17 @@ let app = new Vue({
       },
     ],
     sizes: ["XXL", "XL", "L", "M", "S", "XS", "XXS"],
+    cart: 0,
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
+    removeFromCart: function () {
+      this.cart -= 1;
+    },
+    updateProduct: function (variant) {
+      this.variant = variant;
+    },
   },
 });
