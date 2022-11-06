26046481477910e9d0b5390da04618bf1d7638c5
Sander Fabry <sander.fabry@yucopia.com>
2022-11-06 14:46:26 +0100

    4. List rendering

--- beginner_vue2/intro_to_vue2/main.js
+++ beginner_vue2/intro_to_vue2/main.js
@@ -5,5 +5,17 @@ let app = new Vue({
     inStock: false,
     inventory: 8,
     onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+      },
+    ],
+    sizes: ["XXL", "XL", "L", "M", "S", "XS", "XXS"],
   },
 });
