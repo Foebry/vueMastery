19ea1d3493752a1510c64d67f07b62e12463aefa
Sander Fabry <sander.fabry@yucopia.com>
2022-11-09 22:11:01 +0100

    4. Router basics

--- beginner_vue2/real_world_vue/src/router/index.js
+++ beginner_vue2/real_world_vue/src/router/index.js
@@ -1,28 +1,31 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import EventCreate from '@/views/EventCreate';
+import EventList from '@/views/EventList';
+import EventShow from '@/views/EventShow';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    name: 'event-list',
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/create',
+    name: 'event-create',
+    component: EventCreate,
+  },
+  {
+    path: '/show',
+    name: 'event-show',
+    component: EventShow,
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
 });
