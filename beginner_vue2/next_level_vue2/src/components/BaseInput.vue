212316458797324222d4b6b2da60e63159d58ae6
Sander Fabry <sander.fabry@yucopia.com>
2022-11-11 17:07:46 +0100

    7. Reusable Form Components: BaseInput

---
+++ beginner_vue2/next_level_vue2/src/components/BaseInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :type="type" @input="updateValue" v-bind="$attrs" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    value: [String, Number],
+    type: {
+      type: String,
+      default: 'text',
+    },
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value);
+    },
+  },
+  inheritAttrs: false,
+};
+</script>
+
+<style></style>
--- beginner_vue2/next_level_vue2/src/views/EventCreate.vue
+++ beginner_vue2/next_level_vue2/src/views/EventCreate.vue
@@ -7,31 +7,25 @@
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+      <BaseInput
+        label="Title"
+        v-model="event.title"
+        placeholder="Title"
+        class="field"
+      />
+      <BaseInput
+        label="Description"
+        v-model="event.description"
+        placeholder="Add a description"
+        class="field"
+      />
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
+      <BaseInput
+        label="Location"
+        v-model="event.location"
+        placeholder="Add a location"
+        class="field"
+      />
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
@@ -52,6 +46,7 @@
 import { mapState, mapGetters } from 'vuex';
 import Datepicker from 'vuejs-datepicker';
 import nprogress from 'nprogress';
+import BaseInput from '@/components/BaseInput';
 
 export default {
   computed: {
@@ -73,6 +68,7 @@ export default {
   },
   components: {
     Datepicker,
+    BaseInput,
   },
   methods: {
     async createEvent() {
