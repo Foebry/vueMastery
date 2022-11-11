83ed629ece05a700b904eade68d58f5d69cca351
Sander Fabry <sander.fabry@yucopia.com>
2022-11-11 09:59:22 +0100

    2. mastering vuex orientation

---
+++ beginner_vue2/mastering_vuex/src/views/EventList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <EventCardVue v-for="event in events" :key="event.id" :event="event" />
+    <BaseIcon />
+  </ul>
+</template>
+
+<script>
+import EventCardVue from '@/components/EventCard.vue';
+import EventService from '@/services/EventService';
+export default {
+  components: {
+    EventCardVue,
+  },
+  async created() {
+    const { data, error } = await EventService.getEvents();
+    if (data) {
+      this.events = data;
+    } else if (error) {
+      console.log(error.message);
+    }
+  },
+  data() {
+    return { events: [] };
+  },
+};
+</script>
+
+<style></style>
