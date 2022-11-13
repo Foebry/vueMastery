cca7b4677a4f4499feed03df3134b2447506847d
Sander Fabry <sander.fabry@yucopia.com>
2022-11-13 11:20:20 +0100

    4. Vue Router Essentials

--- beginner_vue3/real-world-vue/src/router/index.ts
+++ beginner_vue3/real-world-vue/src/router/index.ts
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import EventList from "@/views/EventList.vue";
+import AboutView from "@/views/AboutView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "EventList",
+    component: EventList,
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
   },
 ];
 
